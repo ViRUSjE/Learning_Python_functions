2560c19cece3a1e5813f2061275a77cf2ca93842
ViRUSjE <miloszkraska12@gmail.com>
2024-03-01 20:27:17 +0100

    Is divisible

---
+++ Task 12.py
@@ -0,0 +1,10 @@
+def is_divisible_by_4(number):
+    if number % 4 == 0:
+        return True
+    else:
+        return False
+
+print(is_divisible_by_4(12))
+print(is_divisible_by_4(13))
+print(is_divisible_by_4(256))
+print(is_divisible_by_4(1245))
