4ff9fe0342bd6989a53077777cc722445daac176
ViRUSjE <miloszkraska12@gmail.com>
2024-03-02 21:32:27 +0100

    List keys

---
+++ Task 16.py
@@ -0,0 +1,12 @@
+Dict = {'key1': 'value1',
+        'key2': 'value2',
+        'key3': 'value3',
+        'key4': 'value4',
+        'key5': 'value5',
+        'key6': 'value6'
+        }
+
+def list_keys(d):
+    return list(d.keys())
+
+print(list_keys(Dict))
