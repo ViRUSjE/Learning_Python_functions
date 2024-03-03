52e179fc0c9846e6c5f8d3608ef02e424170755e
ViRUSjE <miloszkraska12@gmail.com>
2024-03-03 20:22:23 +0100

    Throw a dice

---
+++ Task 21.py
@@ -0,0 +1,12 @@
+import random
+def d6(num):
+    result = 0
+
+    for i in range(num):
+        a = random.randint(1,6)
+        print(f"Throw result: {a}")
+        result += a
+
+    return f"The sum of throws is {result}"
+
+print(d6(2))
